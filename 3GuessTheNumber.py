c2c6818b6789b9dbc972f0e17ac4525d28316839
PooravK <boomster.work@gmail.com>
2025-05-21 21:29:57 +0530

    Completed program for guess the number game

---
+++ 3GuessTheNumber.py
@@ -0,0 +1,19 @@
+import random
+
+flag = 1
+count = 1
+guessed = random.randint(0, 10)
+
+while(flag):
+    print("Guess a number between 0 and 100: ", end = "")
+    num = int(input())
+
+    if (num > guessed):
+        print("Oops! The number is lower than that")
+        count = count + 1
+    elif (num < guessed):
+        print("Oops! The number is greater than that")
+        count = count + 1
+    else:
+        print("Thats the correct number! You guessed the answer in:", count, "turns!")
+        break
